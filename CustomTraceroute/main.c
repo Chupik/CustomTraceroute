39d9861153083d63bcae8c09a59915e4607cecfe
chupik <Chupik@MacBook-Air-Alexandr.local>
2015-05-16 19:34:46 +0300

    Initial Commit

---
+++ CustomTraceroute/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CustomTraceroute
+//
+//  Created by Alexandr on 16.05.15.
+//  Copyright (c) 2015 Alexandr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
